70a26cbf94bb98e017861bd9987369942f1f4000
myceldigital <128384677+myceldigital@users.noreply.github.com>
2026-04-30 13:48:06 +0100

    Add budgeted inference package

---
+++ src/mythos_harness/budget/__init__.py
@@ -0,0 +1,5 @@
+"""Budgeted inference primitives."""
+
+from mythos_harness.budget.inference_budget import EscalationPolicy, InferenceBudget, SpendDecision
+
+__all__ = ["EscalationPolicy", "InferenceBudget", "SpendDecision"]
