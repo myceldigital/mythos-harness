2c185786ad0f45d55026437d9aafb47f772dbd28
myceldigital <128384677+myceldigital@users.noreply.github.com>
2026-04-30 13:48:35 +0100

    Add expert panel package

---
+++ src/mythos_harness/panels/__init__.py
@@ -0,0 +1,5 @@
+"""Expert-panel branch strategy primitives."""
+
+from mythos_harness.panels.expert_roles import ExpertRole, default_roles_for_domain
+
+__all__ = ["ExpertRole", "default_roles_for_domain"]
